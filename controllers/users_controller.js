f03084d44e68824f655f16ff8710277de8d0293f
Pham Ngoc Long <94893379+LongPN-BE@users.noreply.github.com>
2024-05-08 00:15:02 +0700

    link url

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,7 +4,10 @@ const userData = require('../data/users');
 
 const login = async (req, res) => {
     try {
-        const user = await userData.login(req.body.userName, req.body.passWord);
+        const userName = req.body.username;
+        const passWord = req.body.passWord;
+        if (!userName || !passWord) return res.status(400).send({ code: -1, message: "Vui lòng hãy nhập ký tự. Không đươc bỏ trống" });
+        const user = await userData.login(userName, passWord);
         if (user[0][0].outputNumber == 0) res.status(200).json(user[3][0]);
         else return res.status(404).json({ code: user[0][0].outputNumber, message: user[0][0].outputResult });
     } catch (err) {
