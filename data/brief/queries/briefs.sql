0d4818b93d38d6515e43c88cbe72a5fc27b37ffc
Pham Ngoc Long <94893379+LongPN-BE@users.noreply.github.com>
2024-05-08 08:47:16 +0700

    Update and rename Briefs.sql to briefs.sql

--- data/brief/queries/briefs.sql
+++ data/brief/queries/briefs.sql
@@ -12,13 +12,3 @@ FROM
     dbo.HoSoThamDinh
     INNER JOIN dbo.Employees ON dbo.HoSoThamDinh.nguoi_duyet = dbo.Employees.employees_code
     INNER JOIN dbo.Employees AS Employees_1 ON dbo.HoSoThamDinh.nhan_vien_tiep_nhan_ho_so = Employees_1.employees_code;
-
-select
-    *
-from
-    HoSoThamDinh;
-
-select
-    *
-from
-    View_Xem_Danh_Sach_Ho_So_Tham_Dinh;
