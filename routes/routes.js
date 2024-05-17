e15679f9f5543fe705065fd52c0c4f35a7c38814
Pham Ngoc Long <94893379+LongPN-BE@users.noreply.github.com>
2024-05-17 23:14:57 +0700

    mupload

--- routes/routes.js
+++ routes/routes.js
@@ -80,75 +80,18 @@ const {
 router.get('/criteria/internal_credit_criterias', getAllInternalCreditCriteria);
 router.get('/criteria/root_internal_credit_criterias', getAllRootInternalCreditCriteria);
 router.get('/criteria/sub_internal_credit_criterias', getAllSubInternalCreditCriteria);
+
+
 //------------ API File ------------
 const { initializeApp } = require("firebase/app");
-const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require("firebase/storage");
-const multer = require("multer");
-const dotenv = require('dotenv');
-const fileData = require("../data/files");
+const upload = require("../middleware/multer");
+const firebaseConfig = require("../config/firebase.config");
 
-dotenv.config();
-
-const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    databaseURL: process.env.FIRESTORE_DB_URL,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,
-};
 //Initialize a firebase application
 initializeApp(firebaseConfig);
-
-// Initialize Cloud Storage and get a reference to the service
-const storage = getStorage();
-
 // Setting up multer as a middleware to grab photo uploads
-const upload = multer({ storage: multer.memoryStorage() });
-
-router.post("/files/upload", upload.single("filename"), async (req, res) => {
-    try {
-        const dateTime = giveCurrentDateTime();
-
-        const storageRef = ref(storage, `files/${req.file.originalname + "       " + dateTime}`);
-
-        // Create file metadata including the content type
-        const metadata = {
-            contentType: req.file.mimetype,
-        };
-
-        // Upload the file in the bucket storage
-        const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
-        //by using uploadBytesResumable we can control the progress of uploading like pause, resume, cancel
-
-        // Grab the public url
-        const downloadURL = await getDownloadURL(snapshot.ref);
-
-        console.log('File successfully uploaded.');
-        await fileData.createFile(req.file.originalname, downloadURL, 1, 1, 1, dateTime);
-        const file = await fileData.getAllFiles();
-        console.log({
-            message: 'file uploaded to firebase storage',
-            name: req.file.originalname,
-            type: req.file.mimetype,
-            downloadURL: downloadURL
-        })
-        console.log(file)
-        return res.status(200).send(file);
-    } catch (error) {
-        return res.status(400).send(error.message)
-    }
-});
-
-const giveCurrentDateTime = () => {
-    const today = new Date();
-    const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    const dateTime = date + ' ' + time;
-    return dateTime;
-}
+router.post("/files/upload_firebase", upload.single("filename"), fileController.uploadByFireBase);
+router.post("/files/upload_cloundinary", upload.single("filename"), fileController.uploadByCloudinary);
 
 //-------------------
 module.exports = {
